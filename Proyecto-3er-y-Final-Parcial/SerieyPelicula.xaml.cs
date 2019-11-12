b942a2b1351202466da42bf7cc7961fd898187f6
alumno <alumno@E121>
2019-11-12 12:43:20 -0700

    asdasd

---
+++ Proyecto-3er-y-Final-Parcial/SerieyPelicula.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Proyecto_3er_y_Final_Parcial
+{
+    /// <summary>
+    /// Lógica de interacción para SerieyPelicula.xaml
+    /// </summary>
+    public partial class SerieyPelicula : UserControl
+    {
+        public SerieyPelicula()
+        {
+            InitializeComponent();
+        }
+    }
+}
